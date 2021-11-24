2ebeea345139954f6fbc2f3fb2f8276afdad5ac5
Steven <steven.vasilogianis@gmail.com>
2021-11-24 17:35:04 -0500

    Init

---
+++ pictures/rename.sh
@@ -0,0 +1,8 @@
+IMG_4679_01.jpg bottom-printed.jpg
+IMG_4679.jpg side-printed.jpg
+20211110_0060_01.jpg smurfstrusion.jpg
+IMG_4720.jpg cubbies-mounted-empty.jpg
+IMG_4727.jpg cubbies-mounted-empty-angle-view.jpg
+IMG_4717.jpg cubbies-full.jpg
+IMG_4708.jpg cubby-development.jpg
+20211112_0004.jpg dovetail_test_fit.jpg
